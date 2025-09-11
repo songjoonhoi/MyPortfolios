854b98e40f8c56d03af93ac48a3fd5ebce4f1f13
SongJoonHoi <wnsghl9897@naver.com>
2025-09-11 17:46:33 +0900

    프로젝트 등록 완료 삭제 완료

---
+++ demo/src/main/java/com/example/demo/config/MvcConfig.java
@@ -0,0 +1,24 @@
+package com.example.demo.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Value("${file.upload-dir}")
+    private String uploadDir;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // 사용자가 업로드한 파일 경로
+        registry.addResourceHandler("/uploads/**")
+                .addResourceLocations("file:///" + uploadDir + "/");
+
+        // 프로젝트 내부 기본 이미지, CSS, JS 경로
+        registry.addResourceHandler("/css/**", "/js/**", "/img/**")
+                .addResourceLocations("classpath:/static/css/", "classpath:/static/js/", "classpath:/static/img/");
+    }
+}
